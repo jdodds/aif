299ec2a59de5a449c969c53598e070b69133ce48
Dieter Plaetinck <dieter@plaetinck.be>
2008-11-01 11:23:38 +0100

    install docs also in /home/arch/fifa

--- src/patch-install-cd.sh
+++ src/patch-install-cd.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 SRC_DIR=`dirname $0` # the src directory in the git clone 
-mkdir /home/arch/fifa/
+GIT_DIR=`dirname $SRC_DIR` # the git clone dir itself
+mkdir -p /home/arch/fifa/docs
 cp -ax $SRC_DIR/fifa.sh      /arch/fifa.sh
 cp -ax $SRC_DIR/profiles/*   /home/arch/fifa/
 cp -ax $SRC_DIR/lib          /home/arch/fifa/lib
+cp -ax $GIT_DIR/HOWTO        /home/arch/fifa/docs/
+cp -ax $GIT_DIR/README       /home/arch/fifa/docs/
