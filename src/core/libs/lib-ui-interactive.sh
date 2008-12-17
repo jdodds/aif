c5e7eea26c1ce2305012ab67d88e54477e389ccd
Dieter Plaetinck <dieter@plaetinck.be>
2008-12-17 09:45:15 +0100

    todo update for deletion of dm_mapper volumes

--- src/core/libs/lib-ui-interactive.sh
+++ src/core/libs/lib-ui-interactive.sh
@@ -408,7 +408,7 @@ remove_blockdevice ()
 	target="$part $part_type $part_label"
 	declare target_escaped=${target//\//\\/} # note: apparently no need to escape the '+' sign for sed.
 	declare target_escawk=${target_escaped/+/\\+} # ...but that doesn't count for awk
-	fs_string=`awk "/^$target_escawk / { print \$4}" $TMP_BLOCKDEVICES`
+	fs_string=`awk "/^$target_escawk / { print \$4}" $TMP_BLOCKDEVICES` #TODO: fs_string is the entire line, incl part?
 	debug "Cleaning up partition $part (type $part_type, label $part_label).  It has the following FS's on it: $fs_string"
 	sed -i "/$target_escaped/d" $TMP_BLOCKDEVICES || show_warning "blockdevice removal" "Could not remove partition $part (type $part_type, label $part_label).  This is a bug. please report it"
 	for fs in `sed 's/|/ /g' <<< $fs_string`
