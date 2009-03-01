e9804d56dc5bea86357905f73d093079d515fdd4
Dieter Plaetinck <dieter@plaetinck.be>
2009-03-01 18:07:10 +0100

    better error handling in log/debug functions

--- src/aif.sh
+++ src/aif.sh
@@ -57,8 +57,8 @@ log ()
 {
 	mkdir -p $LOG_DIR || die_error "Cannot create log directory"
 	str="[LOG] `date +"%Y-%m-%d %H:%M:%S"` $@"
-	echo -e "$str" > $LOG
-	[ "$LOG_TO_FILE" = 1 ] && echo -e "$str" >> $LOGFILE
+	echo -e "$str" > $LOG || die_error "Cannot log $str to $LOG"
+	[ "$LOG_TO_FILE" = 1 ] && echo -e "$str" >> $LOGFILE || die_error "Cannot log $str to $LOGFILE" 
 }
 
 # $1 = category. one of MAIN, PROCEDURE, UI, UI-INTERACTIVE, FS, MISC, NETWORK, PACMAN, SOFTWARE
@@ -72,8 +72,8 @@ debug ()
 	if [ "$DEBUG" = "1" ]
 	then
 		str="[DEBUG $1 ] $2"
-		echo -e "$str" > $LOG
-		[ "$LOG_TO_FILE" = 1 ] && echo -e "$str" >> $LOGFILE
+		echo -e "$str" > $LOG || die_error "Cannot debug $str to $LOG"
+		[ "$LOG_TO_FILE" = 1 ] && echo -e "$str" >> $LOGFILE || die_error "Cannot debug $str to $LOGFILE"
 	fi
 }
 
