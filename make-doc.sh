d6b08f0cb077a886bcf4969dec3b75a0c965b703
Dieter Plaetinck <dieter@plaetinck.be>
2009-08-03 23:32:16 +0200

    fixes for hyperlinks

--- make-doc.sh
+++ make-doc.sh
@@ -3,10 +3,10 @@ which markdown &>/dev/null || echo "Need markdown utility!" >&2
 # do we need to sed links?
 # do we need to add section 'avail languages'? like http://wiki.archlinux.org/index.php/Official_Arch_Linux_Install_Guide
 # what about summary and related articles?
-# obfuscate email?
+# obfuscate email? wiki supports mailto dingske
 
 for i in doc/official_installation_guide_??
 do
 	echo $i
-	cat  $i | markdown > $i.html
+	cat  $i | markdown | sed 's|<a href="\([^"]*\)"[^>]*>\([^<]*\)</a>|[\1 \2]|g' > $i.html
 done
