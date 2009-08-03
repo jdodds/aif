7f3916486d3e6391e69cc20fb05a4eab2c118a46
Dieter Plaetinck <dieter@plaetinck.be>
2009-08-03 22:02:51 +0200

    todo updates

--- make-doc.sh
+++ make-doc.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 which markdown &>/dev/null || echo "Need markdown utility!" >&2
+# do we need to sed links?
+# do we need to add section 'avail languages'? like http://wiki.archlinux.org/index.php/Official_Arch_Linux_Install_Guide
+# what about summary and related articles?
+# obfuscate email?
 
 for i in doc/official_installation_guide_??
 do
