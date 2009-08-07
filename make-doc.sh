e3356452586b0d526f06d661d445c5abf06bb0aa
Henning Garus <henning.garus@googlemail.com>
2009-08-07 18:53:58 +0200

    workaround for article summary template expansion limitation

--- make-doc.sh
+++ make-doc.sh
@@ -22,7 +22,7 @@ echo $i
 summary=`sed -n '/<p><strong>Article summary<\/strong><\/p>/, /<p><strong>Related articles<\/strong><\/p>/ p' $i.html | grep -v 'strong'` #TODO strip html tags from summary? (maybe parse from markdown instead). this does not render on the wiki!
 
 echo -e "[[Category:Getting and installing Arch (English)]]\n[[Category:HOWTOs (English)]]\n
-{{Article summary start}}\n{{Article summary text|$summary}}\n{{Article summary heading|Available Languages}}\n
+{{Article summary start}}\n{{Article summary text| 1=$summary}}\n{{Article summary heading|Available Languages}}\n
 {{i18n_entry|English|Official Arch Linux Install Guide}}\n
 {{Article summary heading|Related articles}}\n
 {{Article summary wiki|Beginners Guide}} (If you are new to Arch)\n
