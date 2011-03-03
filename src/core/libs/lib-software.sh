10ed0103b33e984b1c645f256de709706ab60e9f
Dieter Plaetinck <dieter@plaetinck.be>
2011-03-03 18:25:40 +0100

    apply indentation with tabs everywhere

--- src/core/libs/lib-software.sh
+++ src/core/libs/lib-software.sh
@@ -66,10 +66,10 @@ installpkg() {
 # enable glibc locales from rc.conf and build initial locale DB
 target_configure_initial_locale() 
 {
-    for i in $(grep "^LOCALE" ${var_TARGET_DIR}/etc/rc.conf | sed -e 's/.*="//g' -e's/\..*//g'); do
-        sed -i -e "s/^#$i/$i/g" ${var_TARGET_DIR}/etc/locale.gen
-    done
-    target_locale-gen
+	for i in $(grep "^LOCALE" ${var_TARGET_DIR}/etc/rc.conf | sed -e 's/.*="//g' -e's/\..*//g'); do
+		sed -i -e "s/^#$i/$i/g" ${var_TARGET_DIR}/etc/locale.gen
+	done
+	target_locale-gen
 }
 
 
@@ -81,41 +81,41 @@ target_locale-gen ()
 
 target_configure_initcpio () {
 	local ret=0
-        # Give initcpio the encrypt hook when / depends on an encrypted volume
-        # (other encrypted volumes, not related to / don't need the encrypt hook, afaik)
-        # If / also depends on lvm, this way the lvm2 hook will also be included in the right place
-        if get_anchestors_mount ';/;'
-        then
-                hooks=`echo "$ANSWER_DEVICES" | cut -d ' ' -f2 | egrep 'lvm-lv|dm_crypt' | sed -e 's/lvm-lv/lvm2/' -e 's/dm_crypt/encrypt/' | tac`
-                hooks=`echo $hooks` # $hooks is now a space separated, correctly ordered list of needed hooks
-                if [ -n "$hooks" ]
-                then
-                        # for each hook we're about to add, remove it first if it's already in
-                        for hook in $hooks
-                        do
-                                sed -i "/^HOOKS/ s/$hook //" ${var_TARGET_DIR}/etc/mkinitcpio.conf || ret=$?
-                        done
-                        # now add the correctly ordered string
-                        sed -i "/^HOOKS/ s/filesystems/$hooks filesystems/" ${var_TARGET_DIR}/etc/mkinitcpio.conf || ret=$?
-                fi
-        fi
-        # The lvm2 hook however is needed for any lvm LV, no matter the involved mountpoints, so include it if we still need to
-        if grep -q lvm-lv $TMP_BLOCKDEVICES && ! grep -q '^HOOKS.*lvm2'  ${var_TARGET_DIR}/etc/mkinitcpio.conf
-        then
-                sed -i "/^HOOKS/ s/filesystems/lvm2 filesystems/" ${var_TARGET_DIR}/etc/mkinitcpio.conf || ret=$?
-        fi
+	# Give initcpio the encrypt hook when / depends on an encrypted volume
+	# (other encrypted volumes, not related to / don't need the encrypt hook, afaik)
+	# If / also depends on lvm, this way the lvm2 hook will also be included in the right place
+	if get_anchestors_mount ';/;'
+	then
+		hooks=`echo "$ANSWER_DEVICES" | cut -d ' ' -f2 | egrep 'lvm-lv|dm_crypt' | sed -e 's/lvm-lv/lvm2/' -e 's/dm_crypt/encrypt/' | tac`
+		hooks=`echo $hooks` # $hooks is now a space separated, correctly ordered list of needed hooks
+		if [ -n "$hooks" ]
+		then
+			# for each hook we're about to add, remove it first if it's already in
+			for hook in $hooks
+			do
+				sed -i "/^HOOKS/ s/$hook //" ${var_TARGET_DIR}/etc/mkinitcpio.conf || ret=$?
+			done
+			# now add the correctly ordered string
+			sed -i "/^HOOKS/ s/filesystems/$hooks filesystems/" ${var_TARGET_DIR}/etc/mkinitcpio.conf || ret=$?
+		fi
+	fi
+	# The lvm2 hook however is needed for any lvm LV, no matter the involved mountpoints, so include it if we still need to
+	if grep -q lvm-lv $TMP_BLOCKDEVICES && ! grep -q '^HOOKS.*lvm2'  ${var_TARGET_DIR}/etc/mkinitcpio.conf
+	then
+		sed -i "/^HOOKS/ s/filesystems/lvm2 filesystems/" ${var_TARGET_DIR}/etc/mkinitcpio.conf || ret=$?
+	fi
 
-        # if keymap/usbinput are not in mkinitcpio.conf, but encrypt is, we should probably add it
-        if line=`grep '^HOOKS.*encrypt' ${var_TARGET_DIR}/etc/mkinitcpio.conf`
-        then
-                if ! echo "$line" | grep -q keymap
-                then
-                        sed -i '/^HOOKS/ s/encrypt/keymap encrypt/' ${var_TARGET_DIR}/etc/mkinitcpio.conf || ret=$?
-                fi
-                if ! echo "$line" | grep -q usbinput
-                then
-                        sed -i '/^HOOKS/ s/keymap/usbinput keymap/' ${var_TARGET_DIR}/etc/mkinitcpio.conf || ret=$?
-                fi
-        fi
+	# if keymap/usbinput are not in mkinitcpio.conf, but encrypt is, we should probably add it
+	if line=`grep '^HOOKS.*encrypt' ${var_TARGET_DIR}/etc/mkinitcpio.conf`
+	then
+		if ! echo "$line" | grep -q keymap
+		then
+			sed -i '/^HOOKS/ s/encrypt/keymap encrypt/' ${var_TARGET_DIR}/etc/mkinitcpio.conf || ret=$?
+		fi
+		if ! echo "$line" | grep -q usbinput
+		then
+			sed -i '/^HOOKS/ s/keymap/usbinput keymap/' ${var_TARGET_DIR}/etc/mkinitcpio.conf || ret=$?
+		fi
+	fi
 	return $ret
 }
