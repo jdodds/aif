7e793682f8e413b489dad7301359fc0df9994d81
Dieter Plaetinck <dieter@plaetinck.be>
2011-08-14 12:26:54 +0200

    fixup! Refactor source setting

--- src/core/libs/lib-ui-interactive.sh
+++ src/core/libs/lib-ui-interactive.sh
@@ -1346,11 +1346,14 @@ interactive_select_source() {
 			list+=($repo-remote "$repo on remote mirror" OFF)
 		fi
 	done
+	# TODO: enable repositories in pacman.conf on target system based on this
 	warn="Be sure to select [core], that's the only must."
 	[ -d /repo/core ] && warn="Be sure to select at least one [core] location,
 	having [core] available is the only must.  DON'T combine a local repository
 	with remote mirrors unless you know what you're doing (this will cause BROKEN packages)!"
 	ask_checklist "Please select which repositories you want to activate.  $warn.
+	Note that this does not affect pacman.conf on the target system, you can
+	finetune repositories later in the "Configure system" step.
 	If you want to manually mount other repositories first, you can mount them under /repo and rerun this step." 0 "${list[@]}" || return 1
 	repos="${ANSWER_CHECKLIST[@]}"
 	if echo ${repos[@]} | grep -q remote; then
